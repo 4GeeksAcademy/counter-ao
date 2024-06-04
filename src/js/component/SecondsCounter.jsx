a2937599defecfe785cffd6c69b7cffbfe9d82fb
Alvaroorta1 <165050201+Alvaroorta1@users.noreply.github.com>
2024-06-04 21:45:57 +0000

    tercer commit

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -1,27 +1,28 @@
-import "./SecondsCounter.css" 
+import React from "react"
+const SecondsCounter = ({ seconds }) => {
+  return (
 
-const SecondsCounter = (( seconds )) => {
-return ( 
+    <div className="container my-5">
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-8">
+          <div className="counter-container">
+            <div className="counter-digit">
+              <spam className="icon">&#128338;</spam>
+              <div>
+                <div className="counter-digit">{Math.floor(seconds / 10000) %  10}</div>
+                <div className="counter-digit">{Math.floor(seconds / 1000) %  10}</div>
+                <div className="counter-digit">{Math.floor(seconds / 100) %  10}</div>
+                <div className="counter-digit">{Math.floor(seconds / 10) %  10}</div>
+                <div className="counter-digit">{Math.floor(seconds / 1) %  10}</div>
+                <div className="counter-digit">{seconds}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-<div className="container my-5"> 
-<div className="row justify-content-center"> 
-  <div className="col-12 col-md-8"> 
-     <div className="counter-container"> 
-         <div className="counter-digit"> 
-           <spam className= "icon">&#128338;</spam>  
-          <div> 
- 
-          {  <div className="counter-digit">(Math.floor(seconds / 10000) %  10)</div> 
-             <div className="counter-digit">(Math.floor(seconds / 1000) %  10)</div> 
-             <div className="counter-digit">(Math.floor(seconds / 100) %  10)</div> 
-             <div className="counter-digit">(Math.floor(seconds / 10) %  10)</div> 
-             <div className="counter-digit">(Math.floor(seconds / 1) %  10)</div> 
-             <div className="counter-digit">(seconds)</div>
-           </div> 
-         </div> 
-      </div> 
-    </div> 
-    )
+    </div>
+  )
+}
 
-
-    )
+export default SecondsCounter 
--- src/js/component/main.jsx
+++ src/js/component/main.jsx
@@ -1,13 +1,13 @@
-import ReactDom from "react-dom/client"; 
-import App from "./App.jsx"; 
-import SeconsCounter from "./SecondsCounter.jsx"; 
+import ReactDom from "react-dom/client";
+import App from "./App.jsx";
+import SeconsCounter from "./SecondsCounter.jsx";
 
 
-let seconds = 0; 
+let seconds = 0;
 
-function renderPersonalizado() { 
-ReactDom.createRoot(document.getElementById("root")).render(<SecondsCounter seconds=(seconds) />); 
-seconds++; 
+function renderPersonalizado() {
+    ReactDom.createRoot(document.getElementById("root")).render(<SecondsCounter seconds={seconds} />);
+    seconds++;
 }
 
 setInterval(renderPersonalizado, 1000);
